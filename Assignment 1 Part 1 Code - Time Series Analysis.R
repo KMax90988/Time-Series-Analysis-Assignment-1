61ec6072bf5be6b366642d840650d1c750b85647
KMax90988 <83695459+KMax90988@users.noreply.github.com>
2026-03-30 19:26:43 +0800

    R code analysis Bulk upload, increment 3/3

---
+++ Assignment 1 Part 1 Code - Time Series Analysis.R
@@ -0,0 +1,147 @@
+#Assignment 1 Part 1 Code - Time Series Analysis
+#Maximus Caruso a1821471
+
+#Declaration of libraries that may be used throughout the project:
+library(tsibble)
+library(fable)
+library(feasts)
+library(dplyr)
+library(ggplot2)
+library(tidyr)  
+
+#For coding convenience since only one dataset will be used for this Assignment,
+#let BirthsAndFertilityRatesAnnual be called "Dataset".
+#The following code both sorts the dataset and renames variables for convenience
+#in later stages. It does this by utilising column manipulation.
+
+#This file contains all final code produced for part 1 of this assignment.
+#Basic data sorting and plotting are followed by ACF, PACF, and Differencing
+#analysis, before the file concludes with an ARIMA model and related 
+#analyses.
+
+
+#Extracting raw .csv data:
+Dataset_raw <- read.csv("BirthsAndFertilityRatesAnnual.csv")
+
+#Converting to a long dataframe format, and renaming labels for convenience:
+Dataset_long <- Dataset_raw %>%
+  pivot_longer(
+    cols = -DataSeries,         
+    names_to = "Year",         
+    values_to = "Value",  
+    values_transform = list(Value = as.numeric) 
+  )
+
+#Further fixing the years columns format:
+Dataset_long <- Dataset_long %>%
+  mutate(Year = as.numeric(gsub("X", "", Year)))
+
+#Filtering for desired data values for the assignment, TFR and TLB:
+Dataset_filtered <- Dataset_long %>%
+  filter(DataSeries %in% c("Total Fertility Rate (TFR)", 
+                           "Total Live-Births"))
+
+#Reconverting to a wide dataframe format:
+Dataset <- Dataset_filtered %>%
+  pivot_wider(names_from = DataSeries, values_from = Value)
+
+#Renaming remaining value columns for convenience:
+Dataset <- Dataset %>%
+  rename(
+    TFR = `Total Fertility Rate (TFR)`,
+    TLB = `Total Live-Births`
+  )
+
+#Filtering desired years for part 1 of the assignment:
+Dataset <- Dataset %>%
+  filter(Year >= 1960 & Year <= 2024)
+
+#Converting to a tsibble for later time series analysis:
+ts_data <- Dataset %>%
+  as_tsibble(index = Year)
+
+
+#Plotting TLB:
+ggplot(ts_data, aes(x = Year, y = TLB)) +
+  geom_line() +
+  ggtitle("Total Live Births over Time")
+
+#Plotting TFR:
+ggplot(ts_data, aes(x = Year, y = TFR)) +
+  geom_line() +
+  ggtitle("Total Fertility Rate over Time")
+
+#Below data analysis techniques are finally applied to the data:
+
+#For total Live births:
+
+#Plotting ACF of TLB: 
+ts_data %>%
+  ACF(TLB) %>%
+  autoplot()
+
+#Plotting PACF of TLB:
+ts_data %>%
+  PACF(TLB) %>%
+  autoplot()
+
+#For total fertility rate:
+
+#Plotting ACF for TFR:
+ts_data %>%
+  ACF(TFR) %>%
+  autoplot()
+
+#Plotting PACF for TFR: 
+ts_data %>%
+  PACF(TFR) %>%
+  autoplot()
+
+#Creating differenced series for each value:
+ts_data <- ts_data %>%
+  mutate(
+    diff_TLB = difference(TLB),
+    diff_TFR = difference(TFR)
+  )
+
+#Plotting the resultant data:
+
+#for TLB:
+
+ts_data %>%
+  autoplot(diff_TLB)
+
+#for TFR:
+
+ts_data %>%
+  autoplot(diff_TFR)
+
+
+#Fitting an ARIMA model for TLB, for only 1960 to 2012 for later forecasting:
+model_TLB <- ts_data %>%
+  filter(Year <= 2012) %>%
+  model(ARIMA(TLB))
+
+#Fitting an ARIMA model for TFR, for only 1960 to 2012 for later forecasting:
+model_TFR <- ts_data %>%
+  filter(Year <= 2012) %>%
+  model(ARIMA(TFR))
+
+#Performing residual analysis on the above model: 
+
+#Checking residual autocorrelation for TLB:
+augment(model_TLB) %>%
+  ACF(.resid) %>%
+  autoplot()
+
+#Performing a Portmenteau test to check for independence: 
+augment(model_TLB) %>%
+  features(.resid, portmanteau_tests)
+
+#Repeating the above code for the TFR model:
+augment(model_TFR) %>%
+  ACF(.resid) %>%
+  autoplot()
+
+augment(model_TFR) %>%
+  features(.resid, portmanteau_tests)
